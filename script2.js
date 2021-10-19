52f13d1eb136ea9664ca8c4d1f2cf09148bfa4d8
Chadpaul97 <chadpaul97@gmail.com>
2021-10-19 18:59:14 -0500

    Likes  page first coimmit

---
+++ script2.js
@@ -0,0 +1,26 @@
+var clicks = 0;
+var clicks1 = 0;
+var clicksss = 0;
+
+
+
+
+function onClick() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+};
+
+
+function onClick1() {
+    clicks1 += 1;
+    document.getElementById("clicks1").innerHTML = clicks1;
+};
+
+
+
+
+function onClick2() {
+    clicksss += 1;
+    document.getElementById("clicksss").innerHTML = clicksss;
+};
+
